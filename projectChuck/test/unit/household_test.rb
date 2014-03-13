9ad01220d61183042193bdcc24915fe8e28154de
Dima Yurkanskiy <dyurkans@andrew.cmu.edu>
2014-03-12 22:40:53 -0400

    Added shoulda matchers for household. Noted which ones need to be completed.

--- projectChuck/test/unit/household_test.rb
+++ projectChuck/test/unit/household_test.rb
@@ -28,9 +28,34 @@ class HouseholdTest < ActiveSupport::TestCase
 	should_not allow_value(10).for(:state)
 	should allow_value("CA").for(:state)
 
+	# tests for cell_phone
+	should allow_value("4122683259").for(:cell_phone)
+	should allow_value("412-268-3259").for(:cell_phone)
+	should allow_value("412.268.3259").for(:cell_phone)
+	should allow_value("(412) 268-3259").for(:cell_phone)
+	should allow_value(nil).for(:cell_phone)
+	should_not allow_value("2683259").for(:cell_phone)
+	should_not allow_value("14122683259").for(:cell_phone)
+	should_not allow_value("4122683259x224").for(:cell_phone)
+	should_not allow_value("800-EAT-FOOD").for(:cell_phone)
+	should_not allow_value("412/268/3259").for(:cell_phone)
+	should_not allow_value("412-2683-259").for(:cell_phone)
+
+	#test for family physician
+	should validate_presence_of(:family_physician)
+
+	#tests for insurance policy number
+	should validate_presence_of(:insurance_policy_no)
+
+	#tests for insurance provider
+	should validate_presence_of(:insurance_provider)
+
 	# tests for active
 	should allow_value(true).for(:active)
 	should allow_value(false).for(:active)
 	should_not allow_value(nil).for(:active)
+
+
+	 # Still need should matchers for these :family_physician, :home_phone, :insurance_policy_no, :insurance_provider, :physician_phone
 
 end
