7b36173371cb53ea0f44adadde8adb7e618dca2b
Dima Yurkanskiy <dyurkans@andrew.cmu.edu>
2014-03-14 16:13:50 -0400

    Added tests for teams.

--- projectChuck/test/unit/team_test.rb
+++ projectChuck/test/unit/team_test.rb
@@ -6,5 +6,12 @@ class TeamTest < ActiveSupport::TestCase
 	should belong_to(:bracket)
 	should have_many(:registrations)
 
+	#test max students
+	should allow_value(10).for(:max_students)
+	should_not allow_value(8).for(:max_students)
+	should_not allow_value(nil).for(:max_students)
+
+	#test name
+	should validate_presence_of(:name)
 
 end
