887117e71038e885e69bc2c4cd7935e5d048d5b3
Landen Samantha <slanden@andrew.cmu.edu>
2014-03-16 10:25:02 -0400

    Added tests for physical date and t shirt size

--- projectChuck/test/unit/registration_test.rb
+++ projectChuck/test/unit/registration_test.rb
@@ -24,16 +24,31 @@ class RegistrationTest < ActiveSupport::TestCase
 	should_not allow_value(-1).for(:team_id)
 
 	#test date
-
+	
 	#test physical
 
 	#test physical date
+	should_not allow_value(2.years.ago.to_date).for(:dob)
+	#not sure about this, we need to check how recent physicals must be
+	should allow_value(1.year.ago.to_date).for(:dob)
+	should allow_value(8.months.ago.to_date).for(:dob)
+	should_not allow_value(1.month.from_now).for(:dob)
+	should_not allow_value("bad").for(:dob)
 
 	#test proof_of_insurance
 
 	#test report_card
 
 	#test t_shirt_size
-
+	#note that size 1=small, size 6=xxxl
+	should validate_presence_of(:t_shirt_size)
+	should validate_numericality_of(:t_shirt_size)
+	should allow_value(1).for(:t_shirt_size)
+	should allow_value(6).for(:t_shirt_size)
+	should_not allow_value(0).for(:t_shirt_size)
+	should_not allow_value(-1).for(:t_shirt_size)
+	should_not allow_value(7).for(:t_shirt_size)
+	should_not allow_value(nil).for(:t_shirt_size)
+	should_not allow_value("small").for(:t_shirt_size)
 
 end
