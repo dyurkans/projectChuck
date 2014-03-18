f23caa0fd0669f42843d685d11a755322e7e2653
Louie Kevin <klouie@andrew.cmu.edu>
2014-03-18 17:37:45 -0400

    deleted repository and remade with copied over test files

--- projectChuck/test/test_helper.rb
+++ projectChuck/test/test_helper.rb
@@ -10,4 +10,59 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  def create_student_context
+  	@ed = FactoryGirl.create(:student)
+  end
+
+  def remove_student_context
+  	@ed.delete
+  end
+
+  def create_registration_context
+
+  end 
+
+  def remove_registration_context
+  	@reg_ed.delete
+  end
+
+  def create_team_context
+
+  end
+
+  def remove_team_context
+
+  end
+
+  def create_bracket_context
+
+  end
+
+  def remove_bracket_context
+
+  end
+
+  def create_household_context
+
+  end
+
+  def remove_household_context
+
+  end
+
+  def create_guardian_context
+
+  end
+
+  def remove_guardian_context
+
+  end
+
+  def create_user_context
+
+  end
+
+  def remove_user_context
+
+  end
 end
