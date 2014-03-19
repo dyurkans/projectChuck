50b1bcff0cc0e5b0287053080ceb58b5d00a3ab9
Dima Yurkanskiy <dyurkans@andrew.cmu.edu>
2014-03-18 22:52:39 -0400

    Began student model, but still not finished.

--- projectChuck/app/models/household.rb
+++ projectChuck/app/models/household.rb
@@ -1,6 +1,5 @@
 class Household < ActiveRecord::Base
-  attr_accessible :active, :city, :family_physician, :home_phone, :insurance_policy_no,
-   :insurance_provider, :physician_phone, :state, :street, :zip
+  attr_accessible :active, :city, :family_physician, :home_phone, :insurance_policy_no,:insurance_provider, :physician_phone, :state, :street, :zip
 
    	# Relationships
 	has_many :students
