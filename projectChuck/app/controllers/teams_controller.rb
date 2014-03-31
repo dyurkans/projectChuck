b35b0a067f950fc061c9305d38088e960bdc2c14
Dima Yurkanskiy <dyurkans@andrew.cmu.edu>
2014-03-31 03:28:31 -0400

    generated base controller for teams model

---
+++ projectChuck/app/controllers/teams_controller.rb
@@ -0,0 +1,51 @@
+class TeamsController < ApplicationController
+
+  def new
+    @team = Team.new
+  end
+  
+  def edit
+    @team = Team.find(params[:id])
+  end
+  
+  def index
+    @teams = Team.alphabetical.paginate(:page => params[:page]).per_page(10)
+  
+  end
+  
+  def show
+  	@team = Team.find(params[:id])
+
+
+  end
+  
+  def create
+    @team = Team.new(params[:team])
+    if @team.save
+      # if saved to database
+      flash[:notice] = "Successfully created #{@team.name}."
+      redirect_to @team # go to show team page
+    else
+      # return to the 'new' form
+      render :action => 'new'
+    end
+  end
+  
+  def update
+    @team = Team.find(params[:id])
+    if @team.update_attributes(params[:team])
+      flash[:notice] = "Successfully updated #{@team.name}."
+      redirect_to @team
+    else
+      render :action => 'edit'
+    end
+  end
+  
+  def destroy
+    @team = Team.find(params[:id])
+    @team.destroy
+    flash[:notice] = "Successfully removed #{@team.name} from the Project C.H.U.C.K. System"
+    redirect_to teams_url
+  end
+
+end
