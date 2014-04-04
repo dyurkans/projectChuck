98e850188f6a375f2d45c01df2d64473a802d8fa
Dima Yurkanskiy <dyurkans@andrew.cmu.edu>
2014-04-04 02:31:54 -0400

    Index and show views for tournament are complete. Moving on to form.

--- projectChuck/app/models/tournament.rb
+++ projectChuck/app/models/tournament.rb
@@ -1,4 +1,33 @@
 class Tournament < ActiveRecord::Base
   attr_accessible :end_date, :start_date
   has_many :brackets
+
+  validates_date :start_date, :message => "must be valid date"
+  validates_date :end_date, :message => "must be valid date"
+  validate :valid_dates, :message => "end date must later than start date"
+
+  scope :by_date, order('start_date')
+
+  def name
+  	"Project C.H.U.C.K. #{self.start_date.year}"
+  end
+
+  def valid_dates
+  	return self.end_date > self.start_date
+  end
+
+  def number_of_students
+  	number_of_students = 0
+  	@brackets = self.brackets
+    @teams = []
+    for bracket in @brackets
+    	@teams << bracket.teams
+    end
+  	for team in @teams
+  		for t in team
+  			number_of_students += Registration.where(:team_id => t.id).size()
+  		end
+  	end
+  	return number_of_students
+  end
 end
