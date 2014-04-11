4f706f12fbdf9616ef56ab80835d05b8a283d638
Dima Yurkanskiy <dyurkans@andrew.cmu.edu>
2014-04-10 22:21:41 -0400

    Closed some issues and bugs including alphabetizing lists, selecting only active, etc

--- projectChuck/app/controllers/students_controller.rb
+++ projectChuck/app/controllers/students_controller.rb
@@ -10,8 +10,7 @@ class StudentsController < ApplicationController
   def index
     @students = Student.active.alphabetical.paginate(:page => params[:page]).per_page(10)
     @inactive_students = Student.inactive.alphabetical.paginate(:page => params[:page]).per_page(10)
-    @all_students = @students + @inactive_students
-
+    @all_students = Student.alphabetical.paginate(:page => params[:page]).per_page(15)
   end
   
   def show
