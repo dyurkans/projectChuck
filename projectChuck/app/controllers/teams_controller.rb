8e38eb62cb570f11505c6c4fbbf575bd8a304970
Dima Yurkanskiy <dyurkans@andrew.cmu.edu>
2014-05-01 15:20:23 -0400

    Finalized refactoring of reg_order in all instances. Fixed pagination bug in eligible student list and increased display size from 5 per page to 10.

--- projectChuck/app/controllers/teams_controller.rb
+++ projectChuck/app/controllers/teams_controller.rb
@@ -22,7 +22,7 @@ class TeamsController < ApplicationController
   
   def show
   	@team = Team.find(params[:id])
-    @eligible_students = @team.eligible_students.paginate(:page => params[:page], :per_page => 5)
+    @eligible_students = @team.eligible_students.paginate(:page => params[:page], :per_page => 10)
   	@bracket = Bracket.find_by_id(@team.bracket_id) unless @team.nil?
   	@registrations = @team.registrations
   	@students = @team.students
