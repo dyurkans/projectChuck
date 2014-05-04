9df46548c68a93c6d2d99deb45dd7902305ebdf9
Dima Yurkanskiy <dyurkans@andrew.cmu.edu>
2014-05-03 23:09:33 -0400

    File view link on student details page now functional.

--- projectChuck/config/routes.rb
+++ projectChuck/config/routes.rb
@@ -23,7 +23,7 @@ ProjectChuck::Application.routes.draw do
   match 'signup' => 'users#new', :as => :signup
   match 'logout' => 'sessions#destroy', :as => :logout
   match 'login' => 'sessions#new', :as => :login
-    
+      
   # add custom routes for certain actions
   get 'teams/:id/remove' => 'teams#remove_student', :as => :remove_student  
   get 'brackets/:id/remove' => 'brackets#remove_team', :as => :remove_team
