0f09cd986faf3f3753c5939e33860dad18475c28
Dima Yurkanskiy <dyurkans@andrew.cmu.edu>
2014-05-06 00:40:25 -0400

    Fixed nested student reg form

--- projectChuck/app/models/registration.rb
+++ projectChuck/app/models/registration.rb
@@ -20,8 +20,8 @@ class Registration < ActiveRecord::Base
   
   #Validations
   validate :student_in_allowable_age_range
-  validates_numericality_of :student_id, :only_integer => true, :greater_than => 0
-  validates_numericality_of :team_id, :only_integer => true, :greater_than => 0, :allow_nil => true # needs to be removed later
+  validates_numericality_of :student_id, :only_integer => true, :greater_than => 0, :allow_nil => true
+  validates_numericality_of :team_id, :only_integer => true, :greater_than => 0, :allow_nil => true 
   validates_inclusion_of :active, :in => [true, false], :message => "must be true or false"
   validates_date :physical_date, :on_or_after => lambda { Date.new(Date.today.year-1, 8, 1) }, :on_or_before => lambda { Date.today }, :on_or_before_message => "cannot be in the future"
   validates_numericality_of :t_shirt_size, :allow_blank => false, :allow_nil => false, :only_integer => true, :greater_than_or_equal_to => 0, :less_than => SIZE_LIST.size
