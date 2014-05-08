cd5b46edf4318d191da494436b3e02427a60796e
Ford Shaun <shaunf@andrew.cmu.edu>
2014-05-08 03:36:52 -0400

    changed enivronments so that only production is precompiled

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,8 +29,6 @@ ProjectChuck::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.assets.compile = true
-  
   # Do not compress assets
   config.assets.compress = false
 
