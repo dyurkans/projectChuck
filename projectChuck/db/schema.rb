12d54265465a11ae41c66ff437a9388dda667f8b
Dima Yurkanskiy <dyurkans@andrew.cmu.edu>
2014-05-07 20:18:35 -0400

    Added conditional for demogrpahics when not enough students. generated notes field in teams.

--- projectChuck/db/schema.rb
+++ projectChuck/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140417061207) do
+ActiveRecord::Schema.define(:version => 20140507233512) do
 
   create_table "brackets", :force => true do |t|
     t.integer  "tournament_id"
@@ -96,6 +96,7 @@ ActiveRecord::Schema.define(:version => 20140417061207) do
     t.datetime "created_at",                  :null => false
     t.datetime "updated_at",                  :null => false
     t.string   "coach"
+    t.text     "notes"
   end
 
   create_table "tournaments", :force => true do |t|
