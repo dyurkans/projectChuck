660fc7da65437a499ad2805b05d0958447ecb8d5
Ford Shaun <shaunf@andrew.cmu.edu>
2014-05-16 02:31:34 -0400

    updated the storage directory for uploaded files

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -13,13 +13,13 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
 #   storage :file
-#   storage :s3
+  storage :s3
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-#   def store_dir
-#     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-#   end
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
 
 #   def cache_dir
 #     "#{Rails.root}/tmp/uploads"
