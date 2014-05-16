d52d9ca59ead4a819e86f3c65d59bcc0507fb4a4
Ford Shaun <shaunf@andrew.cmu.edu>
2014-05-16 01:18:23 -0400

    updated avatar uploader config

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -12,18 +12,18 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # include Sprockets::Helpers::IsolatedHelper
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+#   storage :file
 #   storage :s3
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+#   def store_dir
+#     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+#   end
 
-  def cache_dir
-    "#{Rails.root}/tmp/uploads"
-  end
+#   def cache_dir
+#     "#{Rails.root}/tmp/uploads"
+#   end
   
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
@@ -34,40 +34,40 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process :scale => [200, 300]
+#   process :scale => [200, 300]
   
-  def scale(width, height)
-    # do something
-  end
+#   def scale(width, height)
+#     # do something
+#   end
 
   # Create different versions of your uploaded files:
   version :thumb do
     process :scale => [50, 50]
   end
   
-#   version :print do
-#     version :thumb do
-#       process :resize_to_fit => [32, 32]
-#     end
-#     version :preview do
-#       process :resize_to_fit => [256, 256]
-#     end
-#     version :full do
-#       process :resize_to_fit => [2048, 2048]
-#     end
-#   end
-#  
-#   version :web do
-#     version :thumb do
-#       process :resize_to_fit => [32, 32]
-#     end
-#     version :preview do 
-#       process :resize_to_fit => [128, 128]
-#     end
-#     version :full do 
-#       process :resize_to_fit => [1024, 768]
-#     end
-#   end
+  version :print do
+    version :thumb do
+      process :scale => [32, 32]
+    end
+    version :preview do
+      process :scale => [256, 256]
+    end
+    version :full do
+      process :scale => [2048, 2048]
+    end
+  end
+ 
+  version :web do
+    version :thumb do
+      process :scale => [32, 32]
+    end
+    version :preview do 
+      process :scale => [128, 128]
+    end
+    version :full do 
+      process :scale => [1024, 768]
+    end
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
