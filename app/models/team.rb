b1063d1d1fea29e18f388044c06cb90acfc8287f
Dmitriy Yurkanskiy <Dima@Dmitriys-MacBook-Pro.local>
2014-06-19 21:17:49 -0400

    Working on team test

--- app/models/team.rb
+++ app/models/team.rb
@@ -27,11 +27,11 @@ class Team < ActiveRecord::Base
   has_many :registrations
   has_many :students, :through => :registrations
 
-  validates_numericality_of :bracket_id, :only_integer => true, :greater_than => 0, :allow_nil => false, :message => "Please create a bracket first."
-  validates_inclusion_of :name, :in => FULL_TEAM_LIST.map{ |t| t[1]}, :message => "must be proper team name"
+  validates_numericality_of :bracket_id, :only_integer => true, :greater_than => 0, :allow_nil => false
+  validates_inclusion_of :name, :in => FULL_TEAM_LIST.map{ |t| t[1]}
   validates_numericality_of :max_students, :only_integer => true, :greater_than => 4, :less_than_or_equal_to => 10, :allow_blank => false, :allow_nil => false
-  validates_format_of :coach_cell, :with => /^\(?\d{3}\)?[-. ]?\d{3}[-.]?\d{4}$/, :message => "Should be 10 digits (area code needed) and separated with dashes only", :allow_blank => true, :allow_nil => true
-  validates_format_of :assistant_coach_cell, :with => /^\(?\d{3}\)?[-. ]?\d{3}[-.]?\d{4}$/, :message => "Should be 10 digits (area code needed) and separated with dashes only", :allow_blank => true, :allow_nil => true
+  validates_format_of :coach_cell, :with => /^\(?\d{3}\)?[-. ]?\d{3}[-.]?\d{4}$/, :allow_blank => true, :allow_nil => true
+  validates_format_of :assistant_coach_cell, :with => /^\(?\d{3}\)?[-. ]?\d{3}[-.]?\d{4}$/, :allow_blank => true, :allow_nil => true
 
   # max may not always be 10
   validate :max
